66fff06e3c0f3ba302e23e0062812ec3def143c1
coalman321 <cjtucker321@gmail.com>
2023-01-16 08:58:45 -0500

    get the hostname in C++ and add the PID list topic

--- ros2_launch_service/launch_service_c/include/launch_service_c/launch_manager_c.h
+++ ros2_launch_service/launch_service_c/include/launch_service_c/launch_manager_c.h
@@ -4,29 +4,43 @@
 #include <string>
 #include <iostream>
 #include <tinyxml2.h>
+
 #include <pybind11/embed.h>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
-#include <launch_msgs/action/bringup_end.hpp>
-#include <launch_msgs/srv/bringup_status.hpp>
-#include <launch_msgs/action/bringup_start.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
+
+// Message headers
+#include <launch_msgs/action/bringup_end.hpp>
+#include <launch_msgs/msg/list_launch.hpp>
+#include <launch_msgs/action/bringup_start.hpp>
 
 namespace launch_manager {
     class LaunchManager: public rclcpp::Node {
-        private:
-            // Map the PIDs of the launch files to the tuple of their required subscriptions and their status
-            std::map<int, std::vector<std::tuple<rclcpp::GenericSubscription::SharedPtr, bool>>> bringup_listeners;
-            // Action servers and services
-            rclcpp::Service<launch_msgs::srv::BringupStatus>::SharedPtr bringup_status;
-            rclcpp_action::Server<launch_msgs::action::BringupEnd>::SharedPtr bringup_end;
-            rclcpp_action::Server<launch_msgs::action::BringupStart>::SharedPtr bringup_start;
-            // Binding methods (*This is why namespaces and OOP are shit and C is good* :P)
-            rclcpp_action::GoalResponse handle_bringup_goal (const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const launch_msgs::action::BringupStart::Goal> goal);
-            rclcpp_action::CancelResponse handle_bringup_cancel (const std::shared_ptr<rclcpp_action::ServerGoalHandle<launch_msgs::action::BringupStart>> goal_handle);
-            void handle_bringup_accepted (const std::shared_ptr<rclcpp_action::ServerGoalHandle<launch_msgs::action::BringupStart>> goal_handle);
-            // Some form of XML document that contains relivent 
-        public:
-            LaunchManager();
+    private:
+        // Map the PIDs of the launch files to the tuple of their required subscriptions and their status
+        std::map<int, std::vector<std::tuple<rclcpp::GenericSubscription::SharedPtr, bool>>> bringup_listeners;
+
+        // string containing the system hostname
+        std::string hostname;
+        
+        // Action servers and services and topics
+        rclcpp::Publisher<launch_msgs::msg::ListLaunch>::SharedPtr bringup_status;
+        rclcpp_action::Server<launch_msgs::action::BringupEnd>::SharedPtr bringup_end;
+        rclcpp_action::Server<launch_msgs::action::BringupStart>::SharedPtr bringup_start;
+        rclcpp::TimerBase::SharedPtr publish_timer;
+
+        // Binding methods
+        rclcpp_action::GoalResponse handle_bringup_goal (const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const launch_msgs::action::BringupStart::Goal> goal);
+        rclcpp_action::CancelResponse handle_bringup_cancel (const std::shared_ptr<rclcpp_action::ServerGoalHandle<launch_msgs::action::BringupStart>> goal_handle);
+        void handle_bringup_accepted (const std::shared_ptr<rclcpp_action::ServerGoalHandle<launch_msgs::action::BringupStart>> goal_handle);
+        void pub_timer_callback();
+
+        // helper functions
+        const std::string get_hostname();
+
+        // Some form of XML document that contains relivent 
+    public:
+        LaunchManager();
     };
 }
--- ros2_launch_service/launch_service_c/src/launch_manager_c.cpp
+++ ros2_launch_service/launch_service_c/src/launch_manager_c.cpp
@@ -1,27 +1,31 @@
 #include "launch_service_c/launch_manager_c.h"
 
+#define MAX_HOST_LEN 300
+
 using namespace launch_manager;
 using namespace std::placeholders;
-
-/*
-  pybind11::exec("from ros2launch.api import launch_a_launch_file"); // use the Python API
-  std::string pyline = "";
-  pyline += "launch_a_launch_file(";
-  pyline += "launch_file_path='/opt/ros/humble/share/dummy_robot_bringup/launch/dummy_robot_bringup.launch.py',";
-  pyline += "launch_file_arguments=[]";
-  pyline += ")";
-
-  pybind11::exec(pyline); // use the Python API
-  return 0;
-  */
+using namespace std::chrono_literals;
 
 LaunchManager::LaunchManager() : Node("launch_manager"){
+    
+
+    RCLCPP_INFO_STREAM(get_logger(), "Binding to hostname '" << hostname << "'");
+
+    // create the action server
     bringup_start = rclcpp_action::create_server<launch_msgs::action::BringupStart>(
       this,
-      "bringup_start",
+      hostname + "/bringup_start",
       std::bind(&LaunchManager::handle_bringup_goal, this, _1, _2),
       std::bind(&LaunchManager::handle_bringup_cancel, this, _1),
       std::bind(&LaunchManager::handle_bringup_accepted, this, _1));
+
+    // create the service server to stop things
+
+    // create the alive topic
+    bringup_status = create_publisher<launch_msgs::msg::ListLaunch>(hostname + "/launch_status", rclcpp::SystemDefaultsQoS());
+
+    // create the status publish timer
+    publish_timer = create_wall_timer(2s, std::bind(&LaunchManager::pub_timer_callback, this));
 }
 
 rclcpp_action::GoalResponse LaunchManager::handle_bringup_goal (const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const launch_msgs::action::BringupStart::Goal> goal){
@@ -33,28 +37,70 @@ rclcpp_action::CancelResponse LaunchManager::handle_bringup_cancel (const std::s
 }
 
 void LaunchManager::handle_bringup_accepted (const std::shared_ptr<rclcpp_action::ServerGoalHandle<launch_msgs::action::BringupStart>> goal_handle) {    
-        auto result = std::make_shared<launch_msgs::action::BringupStart::Result>();
-        uint16_t pid = fork();
-    if(pid == 0)
-    {
-        pybind11::scoped_interpreter guard{}; // start the interpreter and keep it alive
-        pybind11::exec("from ros2launch.api import launch_a_launch_file"); // use the Python API
-        std::string pyline = "";
-        pyline += "launch_a_launch_file(";
-        pyline += "launch_file_path='";
+    auto result = std::make_shared<launch_msgs::action::BringupStart::Result>();
+    uint16_t pid = fork();
+
+    // make sure this is the child thread created during the fork
+    if(pid == 0) {
+        // create the package path
+        std::string packagePath;
         if(goal_handle->get_goal()->launch_package.find('/') != std::string::npos)
-            pyline += goal_handle->get_goal()->launch_package;
+            packagePath = goal_handle->get_goal()->launch_package;
         else
-            pyline += ament_index_cpp::get_package_share_directory(goal_handle->get_goal()->launch_package) + "/launch";
-        pyline += "/";
-        pyline += goal_handle->get_goal()->launch_file;
-        pyline += "', launch_file_arguments=[]";
-        pyline += ")";
-        pybind11::exec(pyline); // use the Python API
+            packagePath = ament_index_cpp::get_package_share_directory(
+                goal_handle->get_goal()->launch_package) + "/launch";
+        
+        // invoke the interpreter
+        pybind11::scoped_interpreter guard{}; // start the interpreter and keep it alive
+        pybind11::exec("from ros2launch.api import launch_a_launch_file"); // import the launch API
+        std::string pyline = "launch_a_launch_file(launch_file_path='" + packagePath 
+            + "/" + goal_handle->get_goal()->launch_file + "', launch_file_arguments=[])";
+        pybind11::exec(pyline); // start the requested launch file
+
+        std::cout << "Launch process Ended" << std::endl;
+
     } else {
         result->pid = pid;
-    }
-        std::cout << "Process Completed" << std::endl;
+
+        // begin startup monitoring here
+
+        std::cout << "Parent thread completed" << std::endl;
         goal_handle->succeed(result);
-        std::cout << "Process Ended" << std::endl;
+    }
+}
+
+void LaunchManager::pub_timer_callback(){
+    // create the vector of pids and resize it correctly
+    std::vector<int16_t> pidList;
+    pidList.reserve(bringup_listeners.size());
+
+    // get the list of PIDs
+    for(auto const& imap: bringup_listeners)
+        pidList.push_back(imap.first);
+
+    // create the message
+    launch_msgs::msg::ListLaunch launchesMsg;
+    launchesMsg.pids = pidList;
+
+    // send the status
+    bringup_status->publish(launchesMsg);
+}
+
+const std::string LaunchManager::get_hostname(){
+    // retrieve the system hostname in hopefully MAX_HOST_LEN characters -1 for null term
+    char hostCstr[MAX_HOST_LEN];
+    ::gethostname(hostCstr, MAX_HOST_LEN);
+    
+    std::string hostnameInternal = hostCstr;
+
+    // make sure we have a null termination
+    if(hostnameInternal.length() >= MAX_HOST_LEN){
+        hostnameInternal = "unknown_host";
+        RCLCPP_WARN_STREAM(get_logger(), "Failed to discover system hostname, falling back to default, " << hostnameInternal);
+    } else {
+        // replace the dashes with dots
+        std::replace(hostnameInternal.begin(), hostnameInternal.end(), '-', '_');
+    }
+
+
 }
