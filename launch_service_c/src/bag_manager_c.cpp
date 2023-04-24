74e4336a35eb667768f6d56f0b4b38372e1d94d0
coalman321 <cjtucker321@gmail.com>
2023-04-24 01:10:16 -0400

    formatter time!

--- launch_service_c/src/bag_manager_c.cpp
+++ launch_service_c/src/bag_manager_c.cpp
@@ -7,15 +7,17 @@
 using namespace std::placeholders;
 using namespace std::chrono_literals;
 
-namespace launch_manager {
-    BagManager::BagManager(const std::string & hostname) : Node(hostname + "_bag_manager"){
+namespace launch_manager
+{
+    BagManager::BagManager(const std::string &hostname) : Node(hostname + "_bag_manager")
+    {
         // create the start server
         startService = create_service<launch_msgs::srv::StartBag>(hostname + "/bag_start", std::bind(&BagManager::startServ, this, _1, _2));
 
         // create the stop server
         stopService = create_service<launch_msgs::srv::StopBag>(hostname + "/bag_stop", std::bind(&BagManager::stopServ, this, _1, _2));
 
-        //create the whois server
+        // create the whois server
         whoisService = create_service<launch_msgs::srv::WhoIs>(hostname + "/bag_whois", std::bind(&BagManager::whoisServ, this, _1, _2));
 
         // create the topic
@@ -25,9 +27,9 @@ namespace launch_manager {
         checkTimer = create_wall_timer(1s, std::bind(&BagManager::timerCallback, this));
     }
 
-
     void BagManager::startServ(const std::shared_ptr<launch_msgs::srv::StartBag::Request> request,
-                        std::shared_ptr<launch_msgs::srv::StartBag::Response> response){
+                               std::shared_ptr<launch_msgs::srv::StartBag::Response> response)
+    {
         // Get launch arguments
         std::vector<std::string> argList;
 
@@ -37,7 +39,8 @@ namespace launch_manager {
         argList.push_back(request->file_name);
 
         // puch back each of the topics
-        for (auto topic : request->topics) {
+        for (auto topic : request->topics)
+        {
             argList.push_back(topic.name);
             argList.push_back(topic.type_name);
             argList.push_back(std::to_string(topic.qos_type));
@@ -46,7 +49,8 @@ namespace launch_manager {
         // We have to do some fancy conversions to pass the argument list to execl
         std::vector<const char *> cstrings;
         cstrings.reserve(argList.size());
-        for(size_t i = 0; i < argList.size(); ++i) {
+        for (size_t i = 0; i < argList.size(); ++i)
+        {
             cstrings.push_back(argList[i].c_str());
         }
 
@@ -57,16 +61,19 @@ namespace launch_manager {
 
         // we start the fork
         pid_t pid = fork();
-        
+
         // two processes at this point
-        if(pid == 0){
+        if (pid == 0)
+        {
             // If this call succeeds, it should never return. only child now
-            execv("/proc/self/exe", const_cast<char *const*>(cstrings.data()));
+            execv("/proc/self/exe", const_cast<char *const *>(cstrings.data()));
 
             // execl failed. Print fatal error, abort the child
-            RCLCPP_FATAL(get_logger(), "execl failed: %s", strerror(errno));    
+            RCLCPP_FATAL(get_logger(), "execl failed: %s", strerror(errno));
             std::abort();
-        } else {
+        }
+        else
+        {
             RCLCPP_INFO(get_logger(), "Parent thread begin monitoring on PID %d", pid);
 
             // wait about 1s for the exec to actually fire
@@ -76,10 +83,14 @@ namespace launch_manager {
             bool pidAlive = true;
             pid_t waitStatus = waitpid(pid, &status, WNOHANG);
 
-            if (waitStatus == -1 ) {
+            if (waitStatus == -1)
+            {
                 RCLCPP_FATAL(get_logger(), "Parent process could not contact child %d, err: %s", pid, strerror(errno));
-            } else if (waitStatus > 0){
-                if ( WIFEXITED(status) ) {
+            }
+            else if (waitStatus > 0)
+            {
+                if (WIFEXITED(status))
+                {
                     int exit_code = WEXITSTATUS(status);
                     RCLCPP_FATAL(get_logger(), "Child %d exited on start with code %d", pid, exit_code);
 
@@ -88,17 +99,19 @@ namespace launch_manager {
                     response->err_msg = "Bag died on startup with exit code " + std::to_string(exit_code);
                 }
 
-                if( WIFSTOPPED(status) ){
+                if (WIFSTOPPED(status))
+                {
                     int stop_sig = WSTOPSIG(status);
                     RCLCPP_FATAL(get_logger(), "Child %d stopped with stop signal %d", pid, stop_sig);
 
                     pidAlive = false;
                     response->err_code = launch_msgs::srv::StartBag::Response::ERR_UNKNOWN;
                     response->err_msg = "Bag died on startup with signal " + std::to_string(stop_sig);
-                }   
+                }
             }
 
-            if(pidAlive){
+            if (pidAlive)
+            {
                 RCLCPP_INFO(get_logger(), "Child startup OK");
 
                 pidsMap[pid] = request->file_name;
@@ -110,35 +123,45 @@ namespace launch_manager {
     }
 
     void BagManager::stopServ(const std::shared_ptr<launch_msgs::srv::StopBag::Request> request,
-                        std::shared_ptr<launch_msgs::srv::StopBag::Response> response){
+                              std::shared_ptr<launch_msgs::srv::StopBag::Response> response)
+    {
         // send a sigint to the bag
         RCLCPP_DEBUG(get_logger(), "Killing process with PID %d. . .", request->pid);
 
         // check that the PIDS are in the map.
-        if(pidsMap.find(request->pid) != pidsMap.end()){
+        if (pidsMap.find(request->pid) != pidsMap.end())
+        {
             // if we have it, shut it down
             int err = kill(request->pid, SIGINT);
-        
-            if (err == -1) {
-                if (errno == ESRCH) {
+
+            if (err == -1)
+            {
+                if (errno == ESRCH)
+                {
                     RCLCPP_ERROR(get_logger(), "Process with PID %d does not exist or is already terminating.", request->pid);
 
                     response->err_code = launch_msgs::srv::StopBag::Response::ALR_DEAD;
                     response->err_msg = "PID was already dead";
-                } else if (errno == EPERM) {
+                }
+                else if (errno == EPERM)
+                {
                     RCLCPP_ERROR(get_logger(), "The monitor has no permission to kill process with PID %d.", request->pid);
 
                     response->err_code = launch_msgs::srv::StopBag::Response::BAD_PERM;
                     response->err_msg = "Insufficent permissions to kill PID";
                 }
-            } else {
+            }
+            else
+            {
                 const auto it = pidsMap.find(request->pid);
                 if (it != pidsMap.end())
                     pidsMap.erase(it);
 
                 response->err_code = launch_msgs::srv::StopBag::Response::ERR_NONE;
             }
-        } else {
+        }
+        else
+        {
             RCLCPP_ERROR(get_logger(), "Process with PID %d does not exist or is already terminating.", request->pid);
 
             response->err_code = launch_msgs::srv::StopBag::Response::ALR_DEAD;
@@ -147,44 +170,55 @@ namespace launch_manager {
     }
 
     void BagManager::whoisServ(const std::shared_ptr<launch_msgs::srv::WhoIs::Request> request,
-                        std::shared_ptr<launch_msgs::srv::WhoIs::Response> response){
+                               std::shared_ptr<launch_msgs::srv::WhoIs::Response> response)
+    {
         // work through the requested pids
-        for(auto pid : request->pids){
+        for (auto pid : request->pids)
+        {
 
             // check that the PIDS are in the map.
-            if(pidsMap.find(pid) != pidsMap.end()){
+            if (pidsMap.find(pid) != pidsMap.end())
+            {
                 // if we have it, give the name
                 response->names.push_back(pidsMap[pid]);
-            } else {
+            }
+            else
+            {
                 // otherwise say its dead
                 response->names.push_back("DEAD");
             }
         }
     }
 
-    void BagManager::timerCallback(){
+    void BagManager::timerCallback()
+    {
 
         launch_msgs::msg::ListPids data;
 
-        
-        for (auto it = pidsMap.begin(); it != pidsMap.end(); ) {
+        for (auto it = pidsMap.begin(); it != pidsMap.end();)
+        {
             // make sure our process hasnt died
             pid_t pid = it->first;
 
             bool pidAlive = true;
             int status;
             pid_t pidStatus = waitpid(pid, &status, WNOHANG);
-            if ( pidStatus  == -1 ) {
+            if (pidStatus == -1)
+            {
                 RCLCPP_FATAL(get_logger(), "Parent process could not contact child %d, err: %s", pid, strerror(errno));
-            } else if( pidStatus > 0 ){
-                if ( WIFEXITED(status) ) {
+            }
+            else if (pidStatus > 0)
+            {
+                if (WIFEXITED(status))
+                {
                     int exit_code = WEXITSTATUS(status);
                     RCLCPP_ERROR(get_logger(), "Child %d exited with code %d while running", pid, exit_code);
 
                     pidAlive = false;
                 }
 
-                if( WIFSTOPPED(status) ){
+                if (WIFSTOPPED(status))
+                {
                     int stop_sig = WSTOPSIG(status);
                     RCLCPP_ERROR(get_logger(), "Child %d stopped with stop signal %d while running", pid, stop_sig);
 
@@ -192,12 +226,15 @@ namespace launch_manager {
                 }
             }
 
-            if(pidAlive){
+            if (pidAlive)
+            {
                 // if he still alive, keep em
                 data.pids.push_back(pid);
 
                 it++;
-            } else {
+            }
+            else
+            {
                 // if he dead, bonk em
                 pidsMap.erase(it++);
             }
@@ -206,8 +243,8 @@ namespace launch_manager {
         monitorTopic->publish(data);
     }
 
-    BagManager::~BagManager(){
-
+    BagManager::~BagManager()
+    {
     }
 
 } // namespace launch_manager
