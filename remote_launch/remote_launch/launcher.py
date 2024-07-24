bd4a140edfde5bf5fa993179b5d057953cdd3669
Robert Pafford <19439938+rjp5th@users.noreply.github.com>
2024-07-24 15:02:03 -0400

    Working systemd tracking

--- remote_launch/remote_launch/launcher.py
+++ remote_launch/remote_launch/launcher.py
@@ -11,6 +11,196 @@ from ament_index_python.packages import get_package_share_directory
 
 import uuid
 
+class LinuxProcess:
+    @staticmethod
+    def lookup_state(state: str):
+        state_map = {
+            "R": "Running",
+            "S": "Sleeping",
+            "D": "Uninterruptable (Disk) Sleep",
+            "Z": "Zombie",
+            "T": "Stopped",
+            "t": "Tracing Stop",
+            "W": "Paging",
+            "X": "Dead",
+            "x": "Dead",
+            "K": "Wakekill",
+            "W": "Waking",
+            "P": "Parked",
+            "I": "Idle"
+        }
+        if state in state_map:
+            return state_map[state]
+        else:
+            return f"Unknown ({state})"
+
+    def __init__(self, pid, stat_str):
+        stat_list = stat_str.split(' ')
+        # Need to do some funky reading in case there's a space in the filename path
+        self.pid = int(stat_list[0])
+        if self.pid != pid:
+            raise RuntimeError("Linux reported stats for pid that we did not request!")
+        self.children: 'list[LinuxProcess]' = []
+        self._args = None
+        self._process_str = None
+        self.comm = ' '.join(stat_list[1:-50]).lstrip('(').rstrip(')')
+        self.state = self.lookup_state(stat_list[-50])
+        self.ppid = int(stat_list[-49])
+        #self.pgrp = int(stat_list[-48])
+        #self.session = int(stat_list[-47])
+        #self.tty_nr = int(stat_list[-46])
+        #self.tpgid = int(stat_list[-45])
+        #self.flags = int(stat_list[-44])
+        #self.minflt = int(stat_list[-43])
+        #self.cminflt = int(stat_list[-42])
+        #self.majflt = int(stat_list[-41])
+        #self.cmajflt = int(stat_list[-40])
+        #self.utime = int(stat_list[-39])
+        #self.stime = int(stat_list[-38])
+        #self.cutime = int(stat_list[-37])
+        #self.cstime = int(stat_list[-36])
+        #self.priority = int(stat_list[-35])
+        #self.nice = int(stat_list[-34])
+        #self.num_threads = int(stat_list[-33])
+        #self.itrealvalue = int(stat_list[-32])
+        #self.starttime = int(stat_list[-31])
+        #self.vsize = int(stat_list[-30])
+        #self.rss = int(stat_list[-29])
+        #self.rsslim = int(stat_list[-28])
+        #self.startcode = int(stat_list[-27])
+        #self.endcode = int(stat_list[-26])
+        #self.startstack = int(stat_list[-25])
+        #self.kstkesp = int(stat_list[-24])
+        #self.kstkeip = int(stat_list[-23])
+        #self.signal = int(stat_list[-22])
+        #self.blocked = int(stat_list[-21])
+        #self.sigignore = int(stat_list[-20])
+        #self.sigcatch = int(stat_list[-19])
+        #self.wchan = int(stat_list[-18])
+        #self.nswap = int(stat_list[-17])
+        #self.cnswap = int(stat_list[-16])
+        #self.exit_signal = int(stat_list[-15])
+        #self.processor = int(stat_list[-14])
+        #self.rt_priority = int(stat_list[-13])
+        #self.policy = int(stat_list[-12])
+        #self.delayacct_blkio_ticks = int(stat_list[-11])
+        #self.guest_time = int(stat_list[-10])
+        #self.cguest_time = int(stat_list[-9])
+        #self.start_data = int(stat_list[-8])
+        #self.end_data = int(stat_list[-7])
+        #self.start_brk = int(stat_list[-6])
+        #self.arg_start = int(stat_list[-5])
+        #self.arg_end = int(stat_list[-4])
+        #self.env_start = int(stat_list[-3])
+        #self.env_end = int(stat_list[-2])
+        #self.exit_code = int(stat_list[-1])
+
+    def get_args(self):
+        if self._args is not None:
+            return self._args
+        try:
+            with open(f'/proc/{self.pid}/cmdline', 'rb') as f:
+                args_raw = f.read().decode(errors="replace")
+            self._args = args_raw.split('\0')
+            return self._args
+        except FileNotFoundError:
+            return None
+
+    def get_process_str(self):
+        if self._process_str is not None:
+            return self._process_str
+
+        def escape_arg(arg: str):
+            if any(map(lambda x: x.isspace(), arg)) or "'" in arg or '"' in arg:
+                if '"' in arg:
+                    arg = "'" + arg + "'"
+                else:
+                    arg = '"' + arg + '"'
+            return arg
+        args = self.get_args()
+        if args is not None and len(args) > 0:
+            self._process_str = " ".join(map(escape_arg, args))
+            return self._process_str
+        else:
+            return f"<{self.pid}: cannot read args>"
+
+    def get_ros_name(self):
+        args = self.get_args()
+        if args is None or len(args) == 0:
+            return f"<{self.pid}: cannnot read args>"
+
+        if args[0].endswith("/python3") or args[0].endswith("/python"):
+            # For python ros nodes, print the script rather than just python3
+            exe_path = args[1]
+        else:
+            exe_path = args[0]
+
+        return f"{os.path.basename(exe_path)} ({self.pid})"
+
+def get_service_cgroup():
+    with open('/proc/self/cgroup') as f:
+        groups = f.readlines()
+        if len(groups) > 1:
+            raise RuntimeError("Multiple cgroups found (cgroupv1 system?):\n\t" + "\t".join(groups))
+        elif len(groups) == 0:
+            raise RuntimeError("Process is not part of a cgroup")
+        group = groups[0].strip()
+        hid, controllers, path = group.split(":")
+        if hid != "0" or controllers != "":
+            raise RuntimeError("Invalid cgroup v2 definition (maybe v1 system?)")
+        path = path.strip('/') # Remove both leading and trailing slash for service detection and os.path.join
+        if not path.endswith('.service'):
+            raise RuntimeError(f"Process is not in a service top level cgroup! (found {path})\n"
+                               "Make sure you run this program as a service in --systemd mode!")
+        sysfs_path = os.path.join("/sys/fs/cgroup", path)
+        if not os.path.exists(sysfs_path):
+            raise RuntimeError(f"Could not find cgroup info in expected directory: {sysfs_path}")
+        return sysfs_path
+
+def get_cgroup_processes(cgroup_path):
+    with open(os.path.join(cgroup_path, "cgroup.procs")) as f:
+        strpid_list = f.readlines()
+    cgroup_processes: 'dict[int, LinuxProcess]' = {}
+    self_pid = os.getpid()
+    parent_pid = os.getppid()
+    for pid_str in strpid_list:
+        pid = int(pid_str.strip())
+        if pid == self_pid or pid == parent_pid:
+            continue  # Don't add our own process or the pid of the ros2 run command
+        try:
+            with open(f"/proc/{pid}/stat") as f:
+                lines = f.readlines()
+                if len(lines) != 1:
+                    raise RuntimeError(f"PID {pid} invalid line count: {len(lines)}")
+                stat_str = lines[0].strip()
+                cgroup_processes[pid] = LinuxProcess(pid, stat_str)
+        except FileNotFoundError:
+            print(f"Failed to fetch pid after reported alive: {pid}! (must have just died)... Ignoring")
+            continue
+
+    # Need to consolidate all children processes under their launches
+    direct_children = list(cgroup_processes.values())
+    for child in cgroup_processes.values():
+        if child.ppid == 1 or child.ppid == self_pid:
+            # Direct descendant of this process (or parent died)
+            # Not going to be consolidated
+            continue
+        if child.ppid in cgroup_processes:
+            # We have it in the cgroup process, remove the child from direct_children
+            parent = cgroup_processes[child.ppid]
+            parent.children.append(child)
+            direct_children.remove(child)
+
+    # Return dictionary of PIDs to direct children to speed up removal of active launches
+    return dict(map(lambda x: (x.pid, x), direct_children))
+
+def dump_children(children: 'list[LinuxProcess]', indent=0):
+    for child in children:
+        #print(f'{" " * indent} - {child.pid}: {child.get_process_str()} ({child.state})')
+        print(f'{" " * indent} - {child.get_ros_name()}')
+        dump_children(child.children, indent+4)
+
+
 SUPER_SECRET_LAUNCH_FLAG = "--super-secret-forking-flag"
 SUPER_SECRET_MONITOR_FLAG = "--super-secret-monitoring-flag"
 TIMEOUT = 15.0
@@ -67,14 +257,27 @@ class LaunchData:
         self.args: str = launch["args"]
         self._monitor_subproc = None
         self._launch_subproc = None
+        self.monitor_procinfo: 'LinuxProcess | None' = None
+        self.launch_procinfo: 'LinuxProcess | None' = None
 
     def _clear_monitored_topics(self):
         for topic in self.monitored_topics:
             topic.seen = False
 
-    def refresh_state(self):
+    def refresh_state(self, process_map: 'dict[int, LinuxProcess]'):
         if self._launch_subproc is None and self._monitor_subproc is None:
             return  # Nothing to do, both processes have been cleaned up
+
+        # Extract the process info from the process map if provided
+        if self._launch_subproc is not None and process_map is not None and self._launch_subproc.pid in process_map:
+            self.launch_procinfo = process_map.pop(self._launch_subproc.pid)
+        else:
+            self.launch_procinfo = None
+
+        if self._monitor_subproc is not None and process_map is not None and self._monitor_subproc.pid in process_map:
+            self.monitor_procinfo = process_map.pop(self._monitor_subproc.pid)
+        else:
+            self.monitor_procinfo = None
 
         if self.state == LaunchState.STOPPING or self.state == LaunchState.STOPPING_ERROR:
             # We're just waiting for the processes to clean up, once both are okay, then set state to stopped/error
@@ -228,7 +431,9 @@ def json_encode_default(o):
             "state": o.state,
             "monitored_topics": o.monitored_topics,
             "args": o.args,
-            "is_zombie": o.is_zombie
+            "is_zombie": o.is_zombie,
+            "launch_procinfo": o.launch_procinfo,
+            "monitor_procinfo": o.monitor_procinfo
         }
     elif isinstance(o, LaunchData.LaunchTopic):
         return {
@@ -237,14 +442,27 @@ def json_encode_default(o):
             "is_sensor_data": o.is_sensor_data,
             "seen": o.seen
         }
+    elif isinstance(o, LinuxProcess):
+        return {
+            "pid": o.pid,
+            "ppid": o.ppid,
+            "state": o.state,
+            "ros_name": o.get_ros_name(),
+            "full_name": o.get_process_str(),
+            "children": o.children
+        }
     else:
         raise TypeError(f'Object of type {o.__class__.__name__} '
                         f'is not JSON serializable')
 
 class ParentServer:
-    def __init__(self) -> None:
+    def __init__(self, systemd_mode: bool) -> None:
         self.launches: 'list[LaunchData]' = []
         self.current_file = ""
+        self.systemd_mode = systemd_mode
+
+        if self.systemd_mode:
+            self.cgroup = get_service_cgroup()
 
     def load_file(self, file_path: str):
         if file_path == self.current_file:
@@ -253,7 +471,7 @@ class ParentServer:
         # Mark all launches as zombies and ask them to terminate if running
         for launch in self.launches:
             launch.is_zombie = True
-            launch.refresh_state()
+            launch.refresh_state(None)
             if launch.can_terminate:
                 launch.term_launch()
         # Purge any dead ones
@@ -339,12 +557,12 @@ class ParentServer:
                     self.send_header("Content-type", "text/html")
                     self.end_headers()
 
-                elif req_path == "/restart":
+                elif req_path == "/restart_service":
                     self.send_response(200)
                     self.send_header("Content-type", "text/html")
                     self.end_headers()
 
-                    subprocess.Popen(["sudo", "reboot", "now"])
+                    exit(0)
 
                 else:
                     self.send_error(404)
@@ -366,19 +584,30 @@ class ParentServer:
 
                 # this route allows the webserver to retrieve state
                 elif req_path == "/status":
-                    self.send_response(200)
-                    self.send_header("Access-Control-Allow-Origin", "*")
-                    self.send_header("Content-type", "application/json")
-                    self.end_headers()
+                    # Fetch current process map so we can check for orphans if in systemd mode
+                    if parent_self.systemd_mode:
+                        process_map = get_cgroup_processes(parent_self.cgroup)
+                    else:
+                        process_map = None
 
                     # Refresh the state of all the launch instances
                     for launch_inst in parent_self.launches:
-                        launch_inst.refresh_state()
+                        launch_inst.refresh_state(process_map)
 
                     # Clean up any zombies
                     parent_self._cleanup_dead_zombies()
 
-                    self.wfile.write(json.dumps(parent_self.launches, default=json_encode_default).encode())
+                    status_resp = {
+                        "systemd_mode": parent_self.systemd_mode,
+                        "launches": parent_self.launches,
+                        "orphans": list(process_map.values()) if parent_self.systemd_mode else None
+                    }
+
+                    self.send_response(200)
+                    self.send_header("Access-Control-Allow-Origin", "*")
+                    self.send_header("Content-type", "application/json")
+                    self.end_headers()
+                    self.wfile.write(json.dumps(status_resp, default=json_encode_default).encode())
 
                 # retrives availiable files
                 elif req_path == "/launches":
@@ -426,14 +655,14 @@ class ParentServer:
         # Mark all child launches to di
         for launch in parent_self.launches:
             launch.is_zombie = True
-            launch.refresh_state()
+            launch.refresh_state(None)
             if launch.can_terminate:
                 launch.term_launch()
 
         # Keep looping until all launches are dead
         while len(parent_self.launches) > 0:
             for launch in parent_self.launches:
-                launch.refresh_state()
+                launch.refresh_state(None)
             parent_self._cleanup_dead_zombies()
             time.sleep(0.05)
 
@@ -510,7 +739,6 @@ def do_child_monitor():
 
 # flag handlers and launch integration
 def main():
-
     # Handle the launch startup
     if len(sys.argv) > 2 and sys.argv[1] == SUPER_SECRET_LAUNCH_FLAG:
         os.setpgrp()  # We only want control C to go to the remote launch server
@@ -548,8 +776,11 @@ def main():
 
     # last resort (no args) is webserver
     else:
-        server = ParentServer()
-        pack_path = get_package_share_directory("remote_launch")
+        if len(sys.argv) > 1 and "--systemd" in sys.argv:
+            systemd_mode = True
+        else:
+            systemd_mode = False
+        server = ParentServer(systemd_mode)
         server.do_main()
 
 
